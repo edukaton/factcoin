c7bc5500f73ccf07f8fb464988fc6aa79c80caac
Piotr Peczek <ppeczek@gmail.com>
2018-02-12 14:09:37 +0100

    staticfiles fix

--- factcoin/settings/base.py
+++ factcoin/settings/base.py
@@ -122,7 +122,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATICFILES_LOCATION = 'static'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 from urllib.parse import urlparse
 # from urlparse import urlparse
