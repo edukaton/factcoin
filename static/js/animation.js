b2c55e5e83928350f8fa816c16bffd6b87dc3028
Michał Cybulski <michal@cybulski.me>
2018-02-18 16:31:34 +0100

    Finished flying ball animation.

--- static/js/animation.js
+++ static/js/animation.js
@@ -57,6 +57,36 @@ window.onload = function() {
         }
     }
 
+    class FlyingBall extends Ball {
+        constructor(startingPosition, targetPosition, arriveCallback, radius, color) {
+            super(startingPosition, radius, color);
+            this.targetPosition = targetPosition;
+            this.arriveCallback = arriveCallback;
+        }
+
+        updatePath(frameEvent) {
+            var oldX = this.position.x;
+            var oldY = this.position.y;
+
+            this.position.x = lerp(oldX, this.targetPosition.x, frameEvent.delta);
+            this.position.y = lerp(oldY, this.targetPosition.y, frameEvent.delta);
+
+            super.updatePath();
+            if (this.isFinished() && this.arriveCallback) {
+                this.arriveCallback();
+            }
+        }
+
+        isFinished() {
+            return Math.abs(this.position.x - this.targetPosition.x) < 35
+                && Math.abs(this.position.y - this.targetPosition.y) < 35;
+        }
+
+        deletePath() {
+            this.circle.remove();
+        }
+    }
+
     class BallBlob extends MovableEntity {
         constructor(position, radius, color) {
             super(position);
@@ -86,52 +116,25 @@ window.onload = function() {
         new BallBlob(new paper.Point([maxPosition.x, maxPosition.y]), maxPosition.x / 3.5, new paper.Color('#F3E0AF'))
     ];
 
-    var largeCircle = new Ball(ballBlobs[1].position, 50, new paper.Color('#F3E0AF'));
+    var largeCircle = null;
 
     var allObjects = [
         ballBlobs[0],
-        ballBlobs[1],
-        largeCircle
+        ballBlobs[1]
     ];
 
-    var handle_len_rate = 2.4;
     var circlePaths = [];
     for (var i = 0, l = allObjects.length; i < l; i++) {
         circlePaths.push(allObjects[i].getPath());
     }
 
-
-    // var connections = new paper.Group();
-    // paper.project.activeLayer.appendTop(connections);
-    //
-    // function generateConnections(paths) {
-    //     // Remove the last connection paths:
-    //     connections.children = [];
-    //
-    //     for (var i = 0, l = paths.length; i < l; i++) {
-    //         for (var j = i - 1; j >= 0; j--) {
-    //             var path = metaball(paths[i], paths[j], 0.5, handle_len_rate, 300);
-    //             if (path) {
-    //                 connections.appendTop(path);
-    //                 path.removeOnMove();
-    //             }
-    //         }
-    //     }
-    // }
-    // generateConnections(circlePaths);
-
-    paper.project.view.onFrame = function(event) {
-        var oldX = largeCircle.position.x;
-        var oldY = largeCircle.position.y;
-
-        largeCircle.position.x = lerp(oldX, target.x, event.delta);
-        largeCircle.position.y = lerp(oldY, target.y, event.delta);
-
+    paper.project.view.onFrame = function(frameEvent) {
         for (var i = 0; i < allObjects.length; i++) {
-            allObjects[i].updatePath();
+            allObjects[i].updatePath(frameEvent);
         }
-
-        // generateConnections(circlePaths);
+        if (largeCircle) {
+            largeCircle.updatePath(frameEvent);
+        }
     };
     paper.project.view.onResize = function() {
         var newSize = paper.view.size;
@@ -144,76 +147,14 @@ window.onload = function() {
         console.log("maxPosition: [" + maxPosition.x + ", " + maxPosition.y + "]");
     };
 
-    function metaball(ball1, ball2, v, handle_len_rate, maxDistance) {
-        var center1 = ball1.position;
-        var center2 = ball2.position;
-        var radius1 = ball1.bounds.width / 2;
-        var radius2 = ball2.bounds.width / 2;
-        var pi2 = Math.PI / 2;
-        var d = center1.getDistance(center2);
-        var u1, u2;
-
-        if (radius1 == 0 || radius2 == 0)
-            return;
-
-        if (d > maxDistance || d <= Math.abs(radius1 - radius2)) {
-            return;
-        } else if (d < radius1 + radius2) { // case circles are overlapping
-            u1 = Math.acos((radius1 * radius1 + d * d - radius2 * radius2) /
-                (2 * radius1 * d));
-            u2 = Math.acos((radius2 * radius2 + d * d - radius1 * radius1) /
-                (2 * radius2 * d));
-        } else {
-            u1 = 0;
-            u2 = 0;
-        }
-
-        var angle1 = center2.subtract(center1).getAngleInRadians();
-        var angle2 = Math.acos((radius1 - radius2) / d);
-        var angle1a = angle1 + u1 + (angle2 - u1) * v;
-        var angle1b = angle1 - u1 - (angle2 - u1) * v;
-        var angle2a = angle1 + Math.PI - u2 - (Math.PI - u2 - angle2) * v;
-        var angle2b = angle1 - Math.PI + u2 + (Math.PI - u2 - angle2) * v;
-        var p1a = center1 + getVector(angle1a, radius1);
-        var p1b = center1 + getVector(angle1b, radius1);
-        var p2a = center2 + getVector(angle2a, radius2);
-        var p2b = center2 + getVector(angle2b, radius2);
-
-        // define handle length by the distance between
-        // both ends of the curve to draw
-        var totalRadius = (radius1 + radius2);
-        var d2 = Math.min(v * handle_len_rate, (p1a - p2a).length / totalRadius);
-
-        // case circles are overlapping:
-        d2 *= Math.min(1, d * 2 / (radius1 + radius2));
-
-        radius1 *= d2;
-        radius2 *= d2;
-
-        var path = new paper.Path({
-            segments: [p1a, p2a, p2b, p1b],
-            style: ball1.style,
-            closed: true
-        });
-        var segments = path.segments;
-        segments[0].handleOut = getVector(angle1a - pi2, radius1);
-        segments[1].handleIn = getVector(angle2a + pi2, radius2);
-        segments[2].handleOut = getVector(angle2b - pi2, radius2);
-        segments[3].handleIn = getVector(angle1b + pi2, radius1);
-        return path;
-    }
-
-    function getVector(radians, length) {
-        return new paper.Point({
-            // Convert radians to degrees:
-            angle: radians * 180 / Math.PI,
-            length: length
-        });
-    }
-
     startBallAnimation = function () {
         return new Promise(function (resolve, reject) {
-            setTimeout(resolve, 100, 'foo');
+            if (!largeCircle) {
+                largeCircle = new FlyingBall(ballBlobs[1].position, ballBlobs[0].position, resolve, 50, new paper.Color('#F3E0AF'));
+            }
+        }).then(() => {
+            largeCircle.deletePath();
+            largeCircle = null
         });
     }
 };
